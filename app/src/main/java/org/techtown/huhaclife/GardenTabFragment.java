1b12b14702eb198c83a6e323e46b41e3005c0c52
yunjeong99 <77837098+yunjeong99@users.noreply.github.com>
2021-10-20 13:49:16 +0900

    ff
    ff

--- app/src/main/java/org/techtown/huhaclife/GardenTabFragment.java
+++ app/src/main/java/org/techtown/huhaclife/GardenTabFragment.java
@@ -1,19 +1,53 @@
 package org.techtown.huhaclife;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class GardenTabFragment extends Fragment {
+
+    ImageButton imgBtn_plantCactus, imgBtn_plantAzalea;
+    Dialog dialog_plant;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup viewGroup=(ViewGroup)inflater.inflate(R.layout.garden_tab_fragment,container,false);
-        return viewGroup;
+        ViewGroup view=(ViewGroup)inflater.inflate(R.layout.garden_tab_fragment,container,false);
+
+        imgBtn_plantCactus=(ImageButton)view.findViewById(R.id.imgBtn_plantCactus);
+        imgBtn_plantAzalea=(ImageButton)view.findViewById(R.id.imgBtn_plantAzalea);
+
+        dialog_plant=new Dialog(getActivity().getApplicationContext());
+        dialog_plant.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog_plant.setContentView(R.layout.plant_dialog);
+
+        imgBtn_plantCactus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                PlantDialog plantDialog=new PlantDialog(getActivity());
+                plantDialog.callFunction("선인장");
+
+            }
+        });
+
+        imgBtn_plantAzalea.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                PlantDialog plantDialog=new PlantDialog(getActivity());
+                plantDialog.callFunction("진달래");
+            }
+        });
+
+
+        return view;
     }
+
 }
---
+++ app/src/main/java/org/techtown/huhaclife/PlantDialog.java
@@ -0,0 +1,103 @@
+package org.techtown.huhaclife;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import org.w3c.dom.Text;
+
+public class PlantDialog {
+    private Context context;
+
+    public PlantDialog(Context context){
+        this.context=context;
+    }
+
+    public void callFunction(String plantName){
+        final Dialog dialog=new Dialog(context);
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.plant_dialog);
+        dialog.show();
+
+        ImageView pimage=(ImageView)dialog.findViewById(R.id.img_plant);
+        TextView pname=(TextView)dialog.findViewById(R.id.tv_plantName);
+        TextView pdate=(TextView)dialog.findViewById(R.id.tv_plantDate);
+        TextView planguage=(TextView)dialog.findViewById(R.id.tv_plantLanguage);
+
+        if(plantName.equals("선인장")){
+            pimage.setImageResource(R.drawable.plant_cactus);
+            pname.setText("이름: 선인장");
+            pdate.setText("키운 날짜: 21/10/03");
+            planguage.setText("꽃말: 타오르는 열정");
+        }
+        else if(plantName.equals("진달래")){
+            pimage.setImageResource(R.drawable.palnt_azalea);
+            pname.setText("이름: 진달래");
+            pdate.setText("키운 날짜: 21/10/05");
+            planguage.setText("꽃말: 사랑의 기쁨");
+        }
+
+
+        final Button cancelButton=(Button)dialog.findViewById(R.id.btn_close);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+}
+
+/*
+public class PlantFragmentDialog extends DialogFragment {
+    private Fragment fragment;
+    private static final String TAG="PlantfragmentDialog";
+    private static final String MAIN_MSG="dialog_plant";
+    private String plantMsg;
+
+    public static PlantFragmentDialog newInstance(String mainMsg){
+        Bundle bundle=new Bundle();
+        bundle.putString(MAIN_MSG, mainMsg);
+
+        PlantFragmentDialog fragment=new PlantFragmentDialog();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getArguments()!=null){
+            plantMsg=getArguments().getString(MAIN_MSG);
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        View view=getActivity().getLayoutInflater().inflate(R.layout.plant_dialog,null);
+
+        builder.setView(view);
+        Dialog dialog=builder.create();
+        return dialog;
+    }
+}
+
+
+ */
