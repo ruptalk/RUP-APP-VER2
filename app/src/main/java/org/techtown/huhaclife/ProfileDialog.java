6aec509e6e7ef5c5eb8856e31d436bc18eca4aba
pthdud1123 <pthdud1123@naver.com>
2021-10-21 13:01:46 +0900

    20211021_sy

---
+++ app/src/main/java/org/techtown/huhaclife/ProfileDialog.java
@@ -0,0 +1,58 @@
+package org.techtown.huhaclife;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+
+public class ProfileDialog extends Dialog {
+
+    private TextView camera,album;
+
+    public interface ProfileDialogListener{
+        void CameraClick();
+    }
+
+    private ProfileDialogListener profileDialogListener;
+
+    public ProfileDialog(@NonNull Context context, ProfileDialogListener profileDialogListener) {
+        super(context);
+        this.setContentView(R.layout.profile_dialog);
+        this.profileDialogListener=profileDialogListener;
+    }
+
+    public ProfileDialog(@NonNull Context context, int themeResId) {
+        super(context, themeResId);
+    }
+
+    protected ProfileDialog(@NonNull Context context, boolean cancelable, @Nullable OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        camera=(TextView)findViewById(R.id.tv_camera);
+        album=(TextView)findViewById(R.id.tv_album);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //인터페이스의 함수를 호출하여 변수에 저장된 값들을 ACtivity로 전달
+                profileDialogListener.CameraClick();
+                dismiss();
+                
+
+            }
+        });
+    }
+
+
+
+}
--- app/src/main/java/org/techtown/huhaclife/UserPageActivity.java
+++ app/src/main/java/org/techtown/huhaclife/UserPageActivity.java
@@ -1,24 +1,45 @@
 package org.techtown.huhaclife;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class UserPageActivity extends AppCompatActivity {
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+public class UserPageActivity extends AppCompatActivity implements AutoPermissionsListener {
     private RecyclerView rv_missionlist;
     private MissionRecyclerAdapter adapter;
     private ImageView userprofile;
+    private File file;
+
+
 
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         setContentView(R.layout.activity_userpage);
+
+
         rv_missionlist=(RecyclerView)findViewById(R.id.rv_missionlist);
         userprofile=(ImageView)findViewById(R.id.iv_userprofile);
 
@@ -26,7 +47,27 @@ public class UserPageActivity extends AppCompatActivity {
         userprofile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                ProfileDialog dialog=new ProfileDialog(UserPageActivity.this, new ProfileDialog.ProfileDialogListener() {
+                    @Override
+                    public void CameraClick() {
+
+                        AutoPermissions.Companion.loadAllPermissions(UserPageActivity.this,101);
+                        if (file == null) {
+                            file = createFile();
+                        }
+                        Uri fileUri = FileProvider.getUriForFile(UserPageActivity.this, "org.techtown.huhaclife.fileprovider", file);
+                        Intent intent1 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        intent1.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                        AutoPermissions.Companion.loadAllPermissions(UserPageActivity.this, 101);
+                        if (intent1.resolveActivity(getPackageManager()) != null) {
+                            startActivityForResult(intent1, 101);
+                        }
+
+                    }
+
+                });
+                dialog.show();
+
 
             }
         });
@@ -49,4 +90,45 @@ public class UserPageActivity extends AppCompatActivity {
         adapter.addItem("포인트 환전해보기",1,0);
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==101&&resultCode==RESULT_OK){
+            BitmapFactory.Options options=new BitmapFactory.Options();
+            options.inSampleSize=8;
+            Bitmap bitmap=BitmapFactory.decodeFile(file.getAbsolutePath(),options);
+            userprofile.setImageBitmap(bitmap);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        AutoPermissions.Companion.parsePermissions(this,requestCode,permissions,this);
+
+    }
+    @Override
+    public void onDenied(int i, @NotNull String[] strings) {
+        Toast.makeText(this,"permission denied : "+i,Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onGranted(int i, @NotNull String[] strings) {
+        Toast.makeText(this,"permission granted : "+i,Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    private File createFile(){
+        String filename="capture.jpg";
+        File startDir= Environment.getExternalStorageDirectory();
+        File outFile=new File(startDir,filename);
+
+        return outFile;
+    }
+
+
 }
