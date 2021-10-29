79a850a1651b591bc6e85a2971acf5a0a506272b
yunjeong99 <77837098+yunjeong99@users.noreply.github.com>
2021-10-29 15:38:15 +0900

    20211029_yj
    Alarm 관련 xml, Activity 추가

---
+++ app/src/main/java/org/techtown/huhaclife/AlarmActivity.java
@@ -0,0 +1,94 @@
+package org.techtown.huhaclife;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class AlarmActivity extends AppCompatActivity {
+    String month, day, uid, TAG="AlarmFragment";
+    public int point, changed_point;
+    public Date currentTime;
+    public SimpleDateFormat dayFormat, monthFormat;
+
+    Context context;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView recyclerView=null;
+    AlarmAdapter adapter=null;
+    ArrayList<AlarmItem> arrayList=new ArrayList<>();
+
+    FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+    DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+
+        recyclerView=(RecyclerView)findViewById(R.id.recyceler_view);
+        layoutManager=new LinearLayoutManager(this);
+        ((LinearLayoutManager) layoutManager).setReverseLayout(true);
+        ((LinearLayoutManager) layoutManager).setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter=new AlarmAdapter(context, arrayList);
+
+        //addValueEventListener : 실시간으로 데이터베이스의 값 변화 감지해서
+        // 변화가 있다면 onDataChange를 호출해주므로 전역변수 값도 그때그때 바뀜.
+        uid=user.getUid();
+        databaseReference= FirebaseDatabase.getInstance().getReference();
+        databaseReference.child("User").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserInfo userInfo=snapshot.getValue(UserInfo.class);
+                changed_point=userInfo.getPoint();
+                Log.d(TAG,"point!!!!: "+changed_point);
+
+
+                initDataset(); //오늘 날짜 받아오기
+                Log.d(TAG, month + " " + day);
+
+                arrayList.add(new AlarmItem("1point가 적립되었습니다.",month, day));
+                adapter=new AlarmAdapter(context, arrayList);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initDataset() {
+
+        for(int i=0; i<100; i++){
+            currentTime= Calendar.getInstance().getTime();
+            dayFormat = new SimpleDateFormat("dd", Locale.getDefault());
+            monthFormat = new SimpleDateFormat("MM", Locale.getDefault());
+
+            month = monthFormat.format(currentTime);
+            day = dayFormat.format(currentTime);
+        }
+
+    }
+}
